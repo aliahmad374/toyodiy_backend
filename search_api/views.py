13113b8d9389c2c2389103bfcef6bdaaf75b81d0
aliahamd374 <alioffice374@gmail.com>
2023-07-04 16:03:15 +0500

    add auto search and search part number feature

--- search_api/views.py
+++ search_api/views.py
@@ -142,7 +142,34 @@ class PartsNumberAPI(APIView):
             if part_parameter is not None:
                 part = Parts.objects.filter(part_number=part_parameter)
                 serializer  = PartsSerializer(part,many=True)
-                return Response([{'id':v.get('id'),'part_number':v.get('part_number')} for v in serializer.data])
+                first_matched = [{'id':v.get('id'),'part_number':v.get('part_number'),'engine_power_id':v.get('engine_power_id')} for v in serializer.data[0:1]]
+
+                other_fits_parts = []
+                done_part = []
+                for other_fit in serializer.data[1:]:
+                    item = dict()
+                    
+                    item['id'] = other_fit.get('id')
+                    item['part_number'] = other_fit.get('part_number')
+                    item['engine_power_id'] = other_fit.get('engine_power_id')
+                    if (item['engine_power_id'] != first_matched[0]['engine_power_id'] ) and (item['engine_power_id'] not in done_part):
+                        vehicle_engine_power = VehicleEngine.objects.get(id=item['engine_power_id'])
+                        serializer = VehicleEngineSerializer(vehicle_engine_power)
+                        model_name_id = serializer.data.get('model_id')
+                        item['engine_power'] = serializer.data.get('engine_power')
+                        item['model_id'] = model_name_id
+                        model_name_object = Model.objects.get(id= model_name_id)
+                        model_name_serializer  = ModelSerializer(model_name_object)
+                        item['name'] = model_name_serializer.data.get('model')
+                        
+                        year_id = serializer.data.get('type_year_id')
+                        item['year_id'] = year_id
+                        year_name_object = TypeYear.objects.get(id=year_id)
+                        year_serializer = TypeYearSerializer(year_name_object)
+                        item['year'] = year_serializer.data.get('year')
+                        other_fits_parts.append(item)
+                        done_part.append(item['engine_power_id'])
+                return Response({'first_matched':first_matched,'other fits':other_fits_parts})
         except:
             return Response({'error':'not found'})    
 
@@ -154,8 +181,7 @@ class AutoComplete(APIView):
                 if text_parameter is not None:
                     part = Parts.objects.filter(part_number__istartswith=text_parameter)
                     serializer  = PartsSerializer(part,many=True)
-                    # return Response([{'id':v.get('id'),'part_number':v.get('part_number')} for v in serializer.data])
-                    return Response(serializer.data[:10])
+                    return Response([{'id':v.get('id'),'part_number':v.get('part_number'),'part_name':v.get('part_name')} for v in serializer.data[:10]])
             except:
                 return Response({'error':'not found'})    
 
