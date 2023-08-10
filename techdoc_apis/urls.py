074e99951e34fb545415d78fd16e09d04a0f30a3
aliahamd374 <alioffice374@gmail.com>
2023-08-10 16:19:06 +0500

    add all part categories and subcategories

--- techdoc_apis/urls.py
+++ techdoc_apis/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('find_articles/',views.find_articles_from_categories,name='article'),
     path('article_info/',views.find_article_information,name='article_info'),
     path('autoCompletesuggestions/',views.AutoCompleteSuggestions,name='autoCompletesuggestions'),
+    path('vinumber/',views.SearchByVINNumber,name='vinumber'),
+    path('allparts/',views.allpartCategories,name='allparts'),
+    path('allpartssubcategories/',views.allpartSubCategories,name='allpartssubcategories'),
     
 
 ]
