ebdf2d059613d1eccce6bb114e1cce4e5ec04367
aliahamd374 <alioffice374@gmail.com>
2023-11-15 16:21:02 +0500

    add order without login

--- cartorder/urls.py
+++ cartorder/urls.py
@@ -4,6 +4,7 @@ from cartorder import views
 
 urlpatterns = [    
     path('order/',views.CreateOrderView.as_view(),name='order'),
+    path('order_notlogin/',views.CreateNotLoginOrderView.as_view(),name='notloginorder'),
     path('vieworder/',views.SearchOrderView.as_view(),name='vieworder'),
     path('admin/changeorder/',views.ChangeOrderStatusView.as_view(),name='changeorder'),
     path('myorders/',views.MyOrdersView.as_view(),name='myorders'),
