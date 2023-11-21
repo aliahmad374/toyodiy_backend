021494421c155f2a982ff76c11b735bbedfddddf
aliahamd374 <alioffice374@gmail.com>
2023-11-21 12:21:33 +0500

    add daraja payment tables in db

--- cartorder/views.py
+++ cartorder/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from account.renderers import UserRenderer
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 import jwt
-from .serializers import CartOrderSerializer,CartProductSerializer
+from .serializers import CartOrderSerializer,CartProductSerializer,PaymentResponseSerializer,PaymentCallbackSerializer
 # Create your views here.
 from rest_framework_simplejwt.authentication import JWTAuthentication
 import random
@@ -16,6 +16,9 @@ from cartorder.utils import Util
 # Create your views here.
 from rest_framework.decorators import permission_classes, authentication_classes
 from rest_framework.decorators import api_view
+import requests
+from datetime import datetime
+import base64
 
 def send_verification_email(email,name,orderid,usertype):
 
@@ -95,9 +98,9 @@ class CreateNotLoginOrderView(APIView):
             return Response({'error':'something went wrong'},status=status.HTTP_400_BAD_REQUEST)
 
 class SearchOrderView(APIView):
-    renderer_classes = [UserRenderer]
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [JWTAuthentication]
+    # renderer_classes = [UserRenderer]
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
     def post(self,request,format=None):
         order_id_id = request.data.get('order_id')
         if order_id_id !=None:
@@ -207,4 +210,123 @@ def get_orders_by_car_linkage_id(request, *args, **kwargs):
     else:
         return Response({'error':'linkageid cannot be null'},status=status.HTTP_400_BAD_REQUEST)
 
-    
+
+def get_access_token():
+    consumer_key = 'exbuAJUwq02smG5sr6OSDpbSlqto0irc'
+    secret_key = 'cTIxXjaGtCdYL4BV'
+    access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+    headers = {'Content-Type': 'application/json'}
+    auth = (consumer_key, secret_key)
+    try:
+        response = requests.get(access_token_url, headers=headers, auth=auth)
+        response.raise_for_status()
+        result = response.json()
+        access_token = result['access_token']
+        return access_token        
+    except requests.exceptions.RequestException as e:
+        return {'error': str(e)}        
+
+@api_view(['POST'])
+def payment_response(request, *args, **kwargs):    
+    access_token = get_access_token()
+    if access_token:
+        amount = request.data.get('amount')
+        phone = request.data.get('phone')
+        if amount!= None and phone!=None:
+            passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+            business_short_code = '174379'
+            process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+            callback_url = 'https://api.darajambili.com/express-payment'
+            timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+            password = base64.b64encode((business_short_code + passkey + timestamp).encode()).decode()
+            party_a = phone
+            party_b = '254708374149'
+            account_reference = 'CompanyXLTD'
+            transaction_desc = 'stkpush test'
+            stk_push_headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access_token
+            }
+
+            stk_push_payload = {
+                'BusinessShortCode': business_short_code,
+                'Password': password,
+                'Timestamp': timestamp,
+                'TransactionType': 'CustomerPayBillOnline',
+                'Amount': amount,
+                'PartyA': party_a,
+                'PartyB': business_short_code,
+                'PhoneNumber': party_a,
+                'CallBackURL': callback_url,
+                'AccountReference': account_reference,
+                'TransactionDesc': transaction_desc
+            }
+            try:
+                response = requests.post(process_request_url, headers=stk_push_headers, json=stk_push_payload)
+                response.raise_for_status()
+
+                data_insetion = dict()
+                data_insetion['merchant_request_id'] = response.json()['MerchantRequestID']
+                data_insetion['checkout_request_id'] = response.json()['CheckoutRequestID']
+                data_insetion['response_code'] = response.json()['ResponseCode']
+                data_insetion['response_description'] = response.json()['ResponseDescription']
+                data_insetion['customer_message'] = response.json()['CustomerMessage']
+
+                if response.json()['ResponseCode'] == "0":
+                    a=1
+                    serializer = PaymentResponseSerializer(data=data_insetion)
+                    if serializer.is_valid(raise_exception=True):
+                        value = serializer.save()
+                        return Response({'success':'success'},status=status.HTTP_200_OK)
+
+
+                else:
+                    return Response({'error':'STK push failed'},status=status.HTTP_400_BAD_REQUEST)
+            except requests.exceptions.RequestException as e:
+                return Response({'error':e},status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error':'amount or phone no is missing'},status=status.HTTP_200_OK)
+    else:    
+        return Response({'error':'invalid access token'},status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def payment_callback(request, *args, **kwargs):
+    received_data = request.data
+    if received_data:
+        callback = received_data['Body']['stkCallback']
+        insertion_callback = dict()
+
+        insertion_callback['merchant_request_id']  = callback['MerchantRequestID']
+
+        insertion_callback['checkout_request_id'] = callback['CheckoutRequestID']
+
+        insertion_callback['result_code'] = callback['ResultCode']
+
+        insertion_callback['result_description'] = callback['ResultDesc']
+
+        
+        try:
+            for loop in callback['CallbackMetadata']['Item']:
+                if loop["Name"] == 'Amount':
+                    insertion_callback['amount'] = loop['Value']
+                elif loop["Name"] == 'MpesaReceiptNumber':
+                    insertion_callback['mpesa_receipt_number'] = loop['Value']
+                elif loop["Name"] == 'TransactionDate':
+                    insertion_callback['tranaction_date'] = loop['Value']        
+                elif loop["Name"] == 'PhoneNumber':
+                    insertion_callback['phonenumber'] = loop['Value'] 
+        except:            
+            insertion_callback['amount'] = None
+            insertion_callback['mpesa_receipt_number'] = "N/A"
+            insertion_callback['tranaction_date'] = "N/A"
+            insertion_callback['phonenumber'] = "N/A"
+        try:    
+            serializer = PaymentCallbackSerializer(data=insertion_callback)
+            if serializer.is_valid(raise_exception=True):
+                value = serializer.save()
+                return Response({'success':'success'},status=status.HTTP_200_OK)    
+        except Exception as E:
+            return Response({'error':str(E)},status=status.HTTP_400_BAD_REQUEST)    
+                
+
